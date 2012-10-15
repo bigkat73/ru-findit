d61de91a98bf4e89e365f703a07ee5601c3af696
Fred Katona <fkatona@healthagen.net>
2012-10-14 21:12:59 -0600

    Indexer in place

--- lib/ru_findit/indexer.rb
+++ lib/ru_findit/indexer.rb
@@ -1,17 +1,30 @@
-class Indexer
+require_relative  'stopper'
 
-  attr_accessor :index
+module RuFindit
 
-  def initialize
-    @index = {}
+  class Indexer
+
+    attr_accessor :index
+
+    def initialize(document_id=nil)
+      @document_id = document_id || 'none'
+      @index = {}
+      @stopper = Stopper.new
+    end
+
+    def add_word(word, index_in_document)
+      index[word] = { @document_id => index_in_document } unless @stopper.matches(word)
+    end
+
+    def remove_word(word)
+      if index.has_key?(word)
+        index[word].delete(@document_id)
+      end
+    end
+
+    def lookup(word)
+      index[word]
+    end
+
   end
-
-  def insert_tokens(tokens)
-    tokens.each do ||
-  end
-
-  def insert_token
-
-  end
-
 end
--- test/indexer_test.rb
+++ test/indexer_test.rb
@@ -1,4 +1,5 @@
 require "test/unit"
+require_relative  '../lib/ru_findit/stopper'
 require_relative  '../lib/ru_findit/indexer'
 
 class IndexerTest < Test::Unit::TestCase
@@ -7,7 +8,7 @@ class IndexerTest < Test::Unit::TestCase
   # to set up fixture information.
   def setup
     # Do nothing
-    @indexer = Indexer.new
+    @indexer = RuFindit::Indexer.new
   end
 
   # Called after every test method runs. Can be used to tear
@@ -18,24 +19,25 @@ class IndexerTest < Test::Unit::TestCase
   end
 
   def test_add_word_to_index
-    @indexer.add_word("fred")
+    @indexer.add_word("fred", "10")
     assert_equal(true, @indexer.index.has_key?("fred"))
   end
 
   def test_lookup_word_in_index
-    @indexer.add_word("fred")
-    assert_equal(true, @indexer.lookup?("fred").class == Hash )
+    @indexer.add_word("fred", "10")
+    assert_equal(true, @indexer.lookup("fred").class == Hash )
   end
 
   def test_remove_word_from_index
-    @indexer.add_word("fred")
+    @indexer.add_word("fred", "10")
     @indexer.remove_word("fred")
-    assert_equal(false, @indexer.word_exists?("fred"))
+    assert_equal(true, @indexer.index.has_key?("fred"))
+    assert_equal(true, @indexer.lookup("fred").empty?)
   end
 
   def test_add_stop_word_to_index
-    @indexer.add_word("and")
-    assert_equal(false, @indexer.word_exists?("and"))
+    @indexer.add_word("and", "11")
+    assert_equal(false, @indexer.index.has_key?("and"))
   end
 
 end
