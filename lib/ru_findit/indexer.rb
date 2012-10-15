c88dd281803444b51df19129e623f2b53114fc6a
Fred Katona <fkatona@healthagen.net>
2012-10-14 20:06:00 -0600

    initial blanks on indexer and test

---
+++ lib/ru_findit/indexer.rb
@@ -0,0 +1,17 @@
+class Indexer
+
+  attr_accessor :index
+
+  def initialize
+    @index = {}
+  end
+
+  def insert_tokens(tokens)
+    tokens.each do ||
+  end
+
+  def insert_token
+
+  end
+
+end
