2571f206e066964f700bd580e2cfad7159c848f1
Fred Katona <fkatona@healthagen.net>
2012-10-17 15:43:31 -0600

    Add requires

--- lib/ru_findit.rb
+++ lib/ru_findit.rb
@@ -1,3 +1,9 @@
+require 'ru_findit/indexer'
+require 'ru_findit/index_catalog'
+require 'ru_findit/stopper'
+require 'ru_findit/tokenizer'
+require 'ru_findit/model/indexer'
+
 module RuFindit
   # your code goes here
 end
