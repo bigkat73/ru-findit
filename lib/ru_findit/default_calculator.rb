a8e61ea5c36d99ff3619becec1ebeb71b866a989
Dan Sharp & Fred Katona <pair+drsharp+fkatona@>
2013-01-02 20:40:57 -0700

    refactoring of caluclating tf idf

--- lib/ru_findit/default_calculator.rb
+++ lib/ru_findit/default_calculator.rb
@@ -1,17 +1,18 @@
+require 'pry'
+require 'pry-nav'
 class DefaultCalculator
 
   def self.calculate(num_docs, documents_word_freq, word_indexes, document_word_count)
-	if num_docs == ''
-	# idf = 1.0 * num_docs / documents_word_freq
-	# scores = word_indexes.map do |k,v| 
-	#   {document_id: k, score: ( (v[:term_frequency]/document_word_count[k]) * idf) }
-	# end
-
-	# scores.sort{|a,b| b[:score] <=> a[:score]}
-    	{document_id: nil, scores: 0}
+    scores = if num_docs == '' && documents_word_freq.empty? && 
+       word_indexes.empty? && document_word_count.empty?
+    	[{document_id: nil, score: 0}]
     else
-      ""
+      idf = 1.0 * num_docs / documents_word_freq
+      word_indexes.map do |k,v| 
+        {document_id: k, score: ( (v[:term_frequency]/document_word_count[k]) * idf) }
+      end
     end
+    scores.sort{|a,b| b[:score] <=> a[:score]}
   end
 
 end
--- test/default_calculate_test.rb
+++ test/default_calculate_test.rb
@@ -7,8 +7,27 @@ describe DefaultCalculator do
 
   describe '0 documents' do
   	it "should calculate the tf_idf score" do
-      DefaultCalculator.calculate('','','','').must_equal({:document_id => nil, :scores => 0})
+      DefaultCalculator.calculate('','','','').must_equal([{:document_id => nil, :score => 0}])
     end
   end
+
+  describe '2 documents' do
+
+    before do
+      indexer = RuFindit::Indexer.new
+      @index_catalog = RuFindit::Catalog.new(indexer)
+      @documents = ["This is a big test.","Another test."]
+      @loaded = @index_catalog.load_documents(@documents)
+      # num_docs, documents_word_freq, word_indexes, document_word_count
+      @word_indexes = { 0 => {:term_frequency => 1.0}, 1 => {:term_frequency => 1.0} }
+      @document_word_count = {0 => 3, 1 => 2}
+    end
+
+    it "should calculate the tf_idf score for word test" do
+      ## TODO pass in less...this is pretty wordy yet
+      DefaultCalculator.calculate(@documents.size,2, @word_indexes, @document_word_count).must_equal([{:document_id => 1, :score => (1/2.0)},{:document_id => 0, :score => (1/3.0)}])
+    end
+  end
+
 
 end
