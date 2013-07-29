25b07c448894a60a3684f99682befc358ecdcacd
Fred Katona <fkatona@healthagen.net>
2013-07-29 16:52:23 -0600

    reformat spacing

--- lib/ru_findit/tokenable.rb
+++ lib/ru_findit/tokenable.rb
@@ -1,39 +1,40 @@
 module RuFindit
+
 	module Tokenable
 
-	  module ClassMethods
-            attr_accessor :document
-            attr_reader   :tokens, :tokenator
-	  end
+		module ClassMethods
+			attr_accessor :document
+			attr_reader   :tokens, :tokenator
+		end
 
-	  def self.included(base)
-	    base.extend ClassMethods
-	  end
+		def self.included(base)
+			base.extend ClassMethods
+		end
 
-	  def tokenize
-	    @tokens ||= document_body.gsub(/\r\n/," ").downcase.split(@tokenator)
-	    cleanse_tokens
-	  end
+		def tokenize
+			@tokens ||= document_body.gsub(/\r\n/," ").downcase.split(@tokenator)
+			cleanse_tokens
+		end
 
-	  def document_body
-	    if self.respond_to?(:body)
-	      self.body
-	    else
-	      self
-	    end
-	  end
+		def document_body
+			if self.respond_to?(:body)
+				self.body
+			else
+				self
+				end
+		end
 
-	  def cleanse_tokens
-	    @tokens.collect!{|token| token.gsub(/\.|\-|\+|,/,'')}
-	  end
+		def cleanse_tokens
+			@tokens.collect!{|token| token.gsub(/\.|\-|\+|,/,'')}
+		end
 
-	  def tokens
-	    tokenize
-	  end
+		def tokens
+			tokenize
+		end
 
-	  def word_count
-	    @word_count ||= tokens.size
-	  end
+		def word_count
+			@word_count ||= tokens.size
+		end
 
 	end
 
